5facb21f0691698a004cf8f4e28d6adb6a134ef7
Vladislav Todorov <vladislav.k.todorov@gmail.com>
2018-11-15 12:37:52 +0200

    Add project files.

---
+++ UMS.APP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UMS.APP.Startup))]
+namespace UMS.APP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
